25e0805a0e5521514a3505f76eee6eccdfdd8961
Dragon <dragon@email.com>
2024-03-26 19:14:44 -0500

    add port to env

--- main.py
+++ main.py
@@ -33,4 +33,4 @@ def get_fecha(cedula: int):
     return {'fecha': fecha}
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=os.getenv("PORT"))
